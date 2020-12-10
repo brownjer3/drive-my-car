c6eb3c8b9c46eeff76ac08de0f7ac55f90a3161a
jerry <brownjer3@gmail.com>
2020-12-10 11:00:34 -0800

    add location data to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201210183617) do
+ActiveRecord::Schema.define(version: 20201210185455) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20201210183617) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "location"
   end
 
 end
