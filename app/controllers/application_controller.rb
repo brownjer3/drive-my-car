f376e97881876ef661a738adb3f812e34a0d6a35
jerry <brownjer3@gmail.com>
2020-12-10 17:23:23 -0800

    refactor User/Show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -52,6 +52,35 @@ class ApplicationController < Sinatra::Base
     def display_date(date)
       date.strftime("%B %d, %Y")
     end
+
+    def owner_name(post)
+      post.user == current_user ? "You need your" : "#{post.user.name} needs their"
+    end
+
+    def car_type(post)
+      "#{post.car_year} #{post.car_make} #{post.car_model}"
+    end
+
+    def time_since_post(time)
+      post_time = time.utc
+      now = Time.now.utc
+      diff_minutes = (now - post_time.to_time )/60
+      if diff_minutes.between?(0,59)
+        "#{diff_minutes.round} minutes ago"
+      elsif diff_minutes.between?(60,1440)
+        "#{(diff_minutes/60).round} hours ago"
+      else
+        diff_days = now.day - post_time.day
+        if diff_days.round == 1
+          "yesterday"
+        elsif diff.between?(1,30)
+          "#{diff} days ago"
+        else
+            months = now.month - post_time.month
+          "#{diff} month(s) ago"
+        end
+      end
+    end
     
   end
 
